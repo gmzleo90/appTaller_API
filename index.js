44b82d947c72aae27c445119dd897642ec91350f
gmzleo90 <lagomezo@ext.falabella.cl>
2023-05-13 18:24:33 -0300

    updating DB confing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express();
-const bd = require('./db/db');
+const db = require('./db/db');
 
 // middlewares
 app.use(bodyParser.json());//parser
