ef2e723a60980a0f5a0f6c04307bc0653435a72a
gmzleo90 <lagomezo@ext.falabella.cl>
2023-05-12 21:45:24 -0300

    create /api/clients/general

--- index.js
+++ index.js
@@ -14,14 +14,15 @@ app.use(cors({         //cors
 
 app.get('/api/clients/general', (req, res) => {
     res.json([{
-        id:1,
-        firstName:'Leonardo',
-        lastName:'Gomez',
-        phone:'1130406130',
-        address:'Paso 1308 Garin',
-        cuit:'20350983555',
-        dni:'35098355',
-}])});
+        id: 1,
+        firstName: 'Leonardo',
+        lastName: 'Gomez',
+        phone: '1123345678',
+        address: 'Pasaje 345 Garin',
+        cuit: '20323254605',
+        dni: '32325460',
+    }])
+});
 
 const PORT = 3000 || process.env.PORT;
 
